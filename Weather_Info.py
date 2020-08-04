9028440f65e4ccf0301cdca75975ed18d2ce1d30
Manu <manuel.pawollek@gmail.com>
2020-08-04 15:50:55 +0200

    Updated the time display

--- Weather_Info.py
+++ Weather_Info.py
@@ -7,14 +7,15 @@ import datetime
 current_time = datetime.datetime.now().time()
 current_hour = str(datetime.datetime.now().time())[:-13]
 print(f"The current time is {current_time}")
-#current_hour = '05'
+#current_hour = '01'
 
 #user_location = "Bad Vilbel"
 #user_location_parsed = "bad+vilbel"
 user_location = input("Please enter your city:> ")
 user_location_parsed = user_location.replace(" ", "+")
+print()
 
-print(f"The weather in {user_location}:")
+print(f"The weather in {user_location} today:")
 
 current_page = requests.get(f"https://www.metcheck.com/HOBBIES/astronomy_forecast.asp?zipcode={user_location_parsed}")
 pageSoup = bs4.BeautifulSoup(current_page.text, "html.parser")
@@ -29,63 +30,43 @@ else:
 
 
 if current_hour == '23':
-	print()
 	n = 0
 	info_today = 5
-elif current_hour == '00':
-	print()
+	time_pos = 1
+if current_hour == '00':
 	n = 0
 	info_today = 4
+	time_pos = 2
 elif current_hour == '01':
-	print()
 	n = 0
 	info_today = 3
+	time_pos = 3
 elif current_hour == '02':
-	print()
 	n = 0
 	info_today = 2
+	time_pos = 4
 elif current_hour == '03':
-	print()
 	n = 0
 	info_today = 1
+	time_pos = 5
 else:
 	n = 23 - int(current_hour) - 1
 	info_today = 6
 	current_hour_display = (23 - int(current_hour))
 	current_hour_display = int(current_hour) + current_hour_display
+	time_pos = 0
 
 skip_value = 12
+display_time = [23, 0, 1, 2, 3, 4]
 init_pos = init_pos + (n * skip_value)
 
-#print(current_hour)
-#print(f"current_hour_dislpay:> {current_hour_display}")
-# make current_hour_display loop over at 24:00
-# idk yet how lul
 
 for i in range(info_today):
-	print(f"Pos {int(current_hour_display)%24 + i}:00:> {relevant_info[init_pos + (i * skip_value)].getText()}")
+	if display_time[time_pos + i] == 23:
+		print(f"Pos {display_time[time_pos + i]}:00:> {relevant_info[init_pos + (i * skip_value)].getText()}")
+	else:
+		print(f"Pos 0{display_time[time_pos + i]}:00:> {relevant_info[init_pos + (i * skip_value)].getText()}")
 	print()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 '''
